e78fb7415b640cab56f79b9f8af758caeb3e661a
Nikolay Abadeev <voood234@gmail.com>
2021-12-02 20:22:07 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-/*
-int bodyMass = 99;
-int inch = 186;
-int bmi = bodyMass / ((inch*2)/100);
-*/
-int bmi = service.calculate(85,186);
+
+        int bmi = service.calculate(85, 186);
         System.out.println(bmi);
+        int bmi1 = service.calculate(99, 170);
+        System.out.println(bmi1);
+        int bmi2 = service.calculate(77, 195);
+        System.out.println(bmi2);
+        int bmi3 = service.calculate(67, 180);
+        System.out.println(bmi3);
+        int bmi4 = service.calculate(79, 186);
+        System.out.println(bmi4);
     }
 }
